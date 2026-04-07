71ce7bdb078245d6dc9b77eb7daca44c78dfb933
Nicky2536 <est.nicolass.guevara@unimilitar.edu.co>
2026-04-07 10:29:32 -0500

    Valores del arreglo random
    Ahora el usuario no es el que ingresa los datos del arreglo, sino que son generados aleatoriamente, el usuario da la cantidad de datos del arreglo

--- src/main/java/Ordenamiento.java
+++ src/main/java/Ordenamiento.java
@@ -1,26 +1,31 @@
-
 import java.util.*;
 
 public class Ordenamiento {
     public static void main(String[] args) {
-       
-   Scanner teclado = new Scanner(System.in); 
-        System.out.println("Ingrese la cantidad de numeros");
-        int cn= teclado.nextInt();
         
-        int arr[] = new int [cn];
+        Scanner teclado = new Scanner(System.in);
+        Random ram = new Random();
+
+        System.out.print("ingrese la cantidad de numeros:");
+        int cn = teclado.nextInt();
+
+        int arr[] = new int[cn];
+
         
-        for( int i = 0; i<cn; i++){
-            System.out.println("ingrese numero: "+ (i+1)+ ":");
-        arr[i] = teclado.nextInt(); }
-         
-      ordenar(arr);
-        System.out.println("El arreglo Ordenado: "+ Arrays.toString(arr));
+        for (int i = 0; i < cn; i++) {
+            arr[i] = ram.nextInt(100); 
+        }
+
+        
+        System.out.println("arreglo original : " + Arrays.toString(arr));
+        ordenar(arr);
+        System.out.println("arreglo ordenado : " + Arrays.toString(arr));
         teclado.close();
-    
+    }
+
     private static void ordenar(int v[]) {
         for (int i = 0; i < v.length; i++) {
-            for (int j = i+1; j < v.length ; j++) {
+            for (int j = i + 1; j < v.length; j++) {
                 if (v[i] > v[j]) {
                     int w = v[j];
                     v[j] = v[i];
@@ -30,5 +35,4 @@ public class Ordenamiento {
         }
     }
 }
-//el usuario da el tamaño de el arr
-//el usuario da los datos de el arr
+//se da una cantidad de num, y l
